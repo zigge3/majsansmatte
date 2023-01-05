b247b466997b057603ce8d40498a46dc684afe9d
Timmy Ylander <timmy.ylander@gmail.com>
2023-01-05 19:26:02 +0100

    Adds whole game

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,63 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import { useEffect, useState } from "react";
+import styles from "../styles/Home.module.css";
+import "bootstrap/dist/css/bootstrap.css";
+import _ from "underscore";
+import gsap from "gsap";
 
 export default function Home() {
+  const [question, setQuestion] = useState([]);
+  const [score, setScore] = useState(0);
+  const [currentAnsw, setCurrentAnsw] = useState("");
+  const [n1, n2] = question;
+  const [vals, setVals] = useState([]);
+  const nextQuestion = () => {
+    const quest = randomQuestion();
+    setCurrentAnsw("");
+    setQuestion(quest);
+    setVals(
+      _.shuffle([quest[0] * quest[1], _.random(1, 100), _.random(1, 100)])
+    );
+  };
+  useEffect(() => {
+    nextQuestion();
+  }, []);
+
+  const correctQuestion = (val) => {
+    setCurrentAnsw(question[0] * question[1]);
+    if (val === question[0] * question[1]) {
+      gsap.fromTo(
+        document.body,
+        { backgroundColor: "green" },
+        { backgroundColor: "white", duration: 1 }
+      );
+
+      setScore(score + 1);
+    } else {
+      gsap.fromTo(
+        document.body,
+        { backgroundColor: "red" },
+        { backgroundColor: "white", duration: 1 }
+      );
+    }
+    setTimeout(nextQuestion, 2000);
+  };
+
   return (
     <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+      <h1>{currentAnsw ? currentAnsw : ` ${n1} * ${n2}`}</h1>
+      <div className="btns">
+        {vals.map((val) => (
+          <button onClick={() => correctQuestion(val)}>{val}</button>
+        ))}
+      </div>
+      <div className="score">{score}</div>
     </div>
-  )
+  );
 }
+
+const randomQuestion = () => {
+  return [
+    Math.floor(Math.random() * 10) + 1,
+    Math.floor(Math.random() * 10) + 1,
+  ];
+};
